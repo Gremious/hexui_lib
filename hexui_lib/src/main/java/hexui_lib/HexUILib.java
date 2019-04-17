ef739dfa6f0be476cfd369743631faf452fda7e8
Gremious <gremious@protonmail.com>
2019-04-17 09:49:55 +0100

    fix main file

--- hexui_lib/src/main/java/hexui_lib/HexUILib.java
+++ hexui_lib/src/main/java/hexui_lib/HexUILib.java
@@ -26,48 +26,17 @@ import hexui_lib.variables.DefaultCustomVariable;
 
 import java.nio.charset.StandardCharsets;
 
-
-//TODO: FIRST THINGS FIRST: RENAME YOUR PACKAGE AND ID NAMES FIRST-THING!!!
-// Right click the package (Open the project pane on the left. Folder with black dot on it. The name's at the very top) -> Refactor -> Rename, and name it whatever you wanna call your mod.
-// Scroll to the very bottom of this file. Change the id string from "hexui_lib:" to "yourModName:" or whatever your heart desires (don't use spaces).
-// In the JSON strings (resources>localization>eng>[all them files] make sure they all go "yourModName:" rather than "hexui_lib". You can ctrl+R to replace in 1 file, or ctrl+shift+r to mass replace in specific files/directories (Be careful.).
-// Start with the DefaultCommon cards - they are the most commented cards since I don't feel it's necessary to put identical comments on every card.
-// After you sorta get the hang of how to make cards, check out the card template which will make your life easier
-
-/*
- * With that out of the way:
- * Welcome to this mildly over-commented Slay the Spire modding base.
- * Use it to make your own mod of any type. - If you want to add any standard in-game content (character,
- * cards, relics), this is a good starting point.
- * It features 1 character with a minimal set of things: 1 card of each type, 1 debuff, 3 types of relics, etc.
- * If you're new to modding, you basically *need* the BaseMod wiki for whatever you wish to add
- * https://github.com/daviscook477/BaseMod/wiki - work your way through with this base.
- * Feel free to use this in any way you like, of course. Happy modding!
- */
-
 @SpireInitializer
 public class HexUILib implements
         PostInitializeSubscriber {
-    // Make sure to implement the subscribers *you* are using (read basemod wiki). Editing cards? EditCardsSubscriber.
-    // Making relics? EditRelicsSubscriber. etc., etc., for a full list and how to make your own, visit the basemod wiki.
     public static final Logger logger = LogManager.getLogger(HexUILib.class.getName());
     private static String modID;
-
-    //This is for the in-game mod settings panel.
+    
     private static final String MODNAME = "HexUI Lib";
     private static final String AUTHOR = "Hexblue"; // And pretty soon - You!
     private static final String DESCRIPTION = "Hexblue's UI Library";
-
-    // =============== INPUT TEXTURE LOCATION =================
-
-
-    //Mod Badge - A small icon that appears in the mod settings menu next to your mod.
+    
     public static final String BADGE_IMAGE = "images/Badge.png";
-
-
-    // =============== /MAKE IMAGE PATHS/ =================
-
-    // =============== /INPUT TEXTURE LOCATION/ =================
 
 
     // =============== SUBSCRIBE, CREATE THE COLOR_GRAY, INITIALIZE =================
@@ -83,32 +52,6 @@ public class HexUILib implements
         logger.info("Done creating the color");
     }
 
-    // ====== NO EDIT AREA ======
-    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
-    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
-
-    public static void setModID(String ID) {
-        modID = ID;
-    }
-
-    public static String getModID() {
-        return modID;
-    }
-
-    private static void pathCheck() {
-        String packageName = HexUILib.class.getPackage().getName();
-        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources");
-        if (!modID.equals("theDefaultDev")) {
-            if (!packageName.equals(getModID())) {
-                throw new RuntimeException("Rename your hexui_lib folder to match your mod ID! " + getModID());
-            }
-            if (!resourcePathExists.exists()) {
-                throw new RuntimeException("Rename your magineerResources folder to match your mod ID! " + getModID() + "Resources");
-            }
-        }
-    }
-    // ====== YOU CAN EDIT AGAIN ======
-
 
     @SuppressWarnings("unused")
     public static void initialize() {
@@ -120,14 +63,8 @@ public class HexUILib implements
 
     // ============== /SUBSCRIBE, CREATE THE COLOR_GRAY, INITIALIZE/ =================
 
-
-    // =============== LOAD THE CHARACTER =================
-
-    // =============== /LOAD THE CHARACTER/ =================
-
-
+    
     // =============== POST-INITIALIZE =================
-
 
     @Override
     public void receivePostInitialize() {
@@ -143,14 +80,37 @@ public class HexUILib implements
         BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
 
     }
-
+    
     // =============== / POST-INITIALIZE/ =================
+    
+    
+    // ====== NO EDIT AREA ======
+    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
+    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
+    
+    public static void setModID(String ID) {
+        modID = ID;
+    }
+    
+    public static String getModID() {
+        return modID;
+    }
+    
+    private static void pathCheck() {
+        String packageName = HexUILib.class.getPackage().getName();
+        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources");
+        if (!modID.equals("theDefaultDev")) {
+            if (!packageName.equals(getModID())) {
+                throw new RuntimeException("Rename your hexui_lib folder to match your mod ID! " + getModID());
+            }
+            if (!resourcePathExists.exists()) {
+                throw new RuntimeException("Rename your magineerResources folder to match your mod ID! " + getModID() + "Resources");
+            }
+        }
+    }
+    
+    // ====== YOU CAN EDIT AGAIN ======
 
-
-
-
-    // this adds "ModName:" before the ID of any card/relic/power etc.
-    // in order to avoid conflicts if any other mod uses the same ID.
     public static String makeID(String idText) {
         return getModID() + ":" + idText;
     }
